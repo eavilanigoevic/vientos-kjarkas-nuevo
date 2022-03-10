2c35a211c278be334073e035eca7af3345913182
eavilanigoevic <eavila_nigoevic@hotmail.com>
2022-03-10 17:42:55 -0400

    first-commit

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="https://reactjs.org">Vientos Kjarkas</a>
+      </header>
+      <main>Lista de Productos</main>
+    </div>
+  );
+}
+
+export default App;
