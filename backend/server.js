d8d3a86ef04be668f6f3f149c57a3e2a5270e4b0
eavilanigoevic <eavila_nigoevic@hotmail.com>
2022-06-23 19:06:22 -0400

    sending html from frontend2

--- backend/server.js
+++ backend/server.js
@@ -7,10 +7,10 @@ const app = express();
 const __dirname = path.resolve();
 console.log(__dirname);
 
-/app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.use(express.static(path.join(__dirname, '/frontend/build')));
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
-); 
+);
 
 /* app.get('/', (req, res) => {
   res.send('Hello from Express!');
